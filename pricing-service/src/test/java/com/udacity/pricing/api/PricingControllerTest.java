cc1fd94c06d40f5e5113d9d4297f90fc8bb8eff3
Plat-26 <lolade.badmus97@gmail.com>
2021-10-23 20:52:30 +0100

    pricing microservice

---
+++ pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -0,0 +1,25 @@
+package com.udacity.pricing.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class PricingControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testGet() {
+
+    }
+}
